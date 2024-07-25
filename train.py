63c0dda15e26261c5fd162570c383f4dc2628221
rishabhpahuja <rishabhpahuja27@gmail.com>
2024-07-25 01:42:25 -0400

    validation function completed

--- train.py
+++ train.py
@@ -15,6 +15,75 @@ from dataloader import *
 from transformer import *
 from config import *
 
+def greedy_decode(model, source, source_mask, tokenzier_src, tokenizer_tgt, max_len, device):
+    
+    sos_idx = tokenizer_tgt.token_to_id('[SOS]')
+    eos_idx = tokenizer_tgt.token_to_id('[EOS]')
+
+    # Precompute the endoder output and resuse it for every token we get from decoder
+    encoder_output = model.encode(source, source_mask)
+    # Initialize the decoder input with the sos token
+    decoder_input = torch.empty(1,1).fill_(sos_idx).type_as(source).to(device)
+
+    while True:
+        if len(decoder_input[0]) == max_len:
+            break
+
+        # build mask for the target (decoder_input)
+        decoder_mask = causal_mask(decoder_input.size(1)).type_as(source_mask).to(device)
+
+        # Get the next token
+        out = model.decode(encoder_output, source_mask, decoder_input, decoder_mask)
+
+        prob = model.predict(out[:,-1])
+
+        _, next_word = torch.max(prob, dim = 1)
+        decoder_input = torch.cat([decoder_input, torch.empty(1,1).type_as(source).fill_(next_word.item()).to(device)])
+
+        if next_word == eos_idx:
+            break
+
+    return decoder_input
+
+
+def run_validation(model, validation_ds, tokenizer_src, tokenizer_tgt, max_len, device, print_msg, global_state, num_examples):
+
+    model.eval()
+    count = 0
+
+    source_texts = []
+    expected = []
+    predicted = []
+
+    console_width = 80
+
+    with torch.no_grad():
+        for batch in validation_ds:
+            count += 1
+            encoder_input = batch['encoder_input'].to(device)
+            encoder_mask = batch['encoder_mask'].to(device)
+
+            assert encoder_input.size(0)==1, "Batch_size must be 1"
+
+            model_out = greedy_decode(model, encoder_input, encoder_mask, tokenizer_src, tokenizer_tgt, max_len, device)
+
+            source_text = batch['src_text'][0]
+            target_text = batch['tgt_text'][0]
+            model_out_text = tokenizer_tgt.decode(model_out.detach().cpu().numpy())
+
+            source_texts.append(source_text)
+            expected.append(target_text)
+            predicted.append(model_out_text)
+
+            print_msg('-'*console_width)
+            print_msg('Source:', source_texts)
+            print_msg('Target:', target_text)
+            print_msg('Predicted:', predicted)
+
+            if count == num_examples:
+                break
+
+
 def get_all_sentences(ds, lang):
     for item in ds:
         yield  item['translation'][lang]
